689e7e43a78feb889b32bcef23db2229b9887ecf
James Yu <jj6yu@uwaterloo.ca>
2016-02-09 16:55:31 -0500

    holla holla cash dolla

--- src/Main.purs
+++ src/Main.purs
@@ -9,15 +9,42 @@ import Control.Monad.Eff.Console
 import Control.Monad.Eff.Exception
 import Control.Monad.Eff.Ref
 
-import RSL.Core as RSL
+import RSL.Core
 import Services.Radredis
+
+n1 :: RSL Int
+n1 = dataFetch (Echo 1 "foo")
+
+n2 :: RSL Int
+n2 = dataFetch (Echo 2 "bar")
+
+n3 :: RSL Int
+n3 = dataFetch (Echo 3 "baz")
+
+n4 :: RSL Int
+n4 = uncachedRequest (Echo 3 "qux")
+
+add2 :: Int -> Int -> Int
+add2 x y = x + y
+
+add3 :: Int -> Int -> Int -> Int
+add3 x y z = x + y + z
+
+sum :: RSL Int
+sum = do
+  s'' <- add2 <$> n1 <*> n2
+  s' <- add3 <$> n1 <*> n3 <*> n4
+  s <- add3 s'' s' <$> n4
+  return s
 
 test :: forall e. Aff ( console :: CONSOLE, ref :: REF | e ) Unit
 test = do
-  env <- liftEff RSL.emptyEnv
-  num <- RSL.runRSL env (RSL.dataFetch (Echo 1 "test"))
+  env <- liftEff emptyEnv
+
+  val <- runRSL env sum
+
   Aff.log "Result is:"
-  Aff.log $ show num
+  Aff.log $ show val
 
 main :: forall e. Eff ( err :: EXCEPTION, console :: CONSOLE, ref :: REF | e) Unit
 main = do
